e08dbfa224e97226825663c89f90380fce7d4d1e
Denis Defreyne <denis.defreyne@stoneship.org>
2008-12-24 11:44:54 +0100

    cleaned up default.rb

--- lib/default.rb
+++ lib/default.rb
@@ -1,7 +1,7 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
-def html_escape(a_string)
-  a_string.gsub('&', '&amp;').gsub('<', '&lt;').gsub('>', '&gt;').gsub('"', '&quot;')
-end
-alias h html_escape
+# Helpers - built-in
+include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::LinkTo
+include Nanoc::Helpers::XMLSitemap
