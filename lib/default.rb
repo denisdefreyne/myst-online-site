7a5b136845a971e0f27304dd5f24752d44c9f41b
Denis Defreyne <denis.defreyne@stoneship.org>
2009-01-11 17:39:40 +0100

    fixed article dates

--- lib/default.rb
+++ lib/default.rb
@@ -5,3 +5,27 @@
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::XMLSitemap
+
+class Fixnum
+
+  def to_mon_s
+    Date::MONTHNAMES[self]
+  end
+
+  def to_abbr_mon_s
+    Date::ABBR_MONTHNAMES[self]
+  end
+
+end
+
+class Numeric
+
+  def ordinal
+    if (10...20).include?(self) then
+      self.to_s + 'th'
+    else
+      self.to_s + %w{th st nd rd th th th th th th}[self % 10]
+    end
+  end
+
+end
