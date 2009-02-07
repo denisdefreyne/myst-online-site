95dbf090c0fd2c7f0c7fa676741ba25a91e705e6
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-07 18:34:34 +0100

    added filter that italicizes proper names

---
+++ lib/filters/italicize_proper_names.rb
@@ -0,0 +1,71 @@
+module MOULSiteFilters
+
+  class ItalicizeProperNamesFilter < Nanoc::Filter
+
+    identifier :italicize_proper_names
+
+    WORDS = [
+      'Cyan Worlds, Inc.',
+      'Cyan Worlds',
+      'CyanChat',
+      'Cyan',
+      'Mudpie',
+      'Untìl Uru',
+      'Myst Online: Uru Live',
+      'Myst Online Restoration Experiment',
+      '"Myst Online"',
+      'Myst Online',
+      'Myst: The Book of Atrus',
+      'Myst: The Book of Ti\'ana',
+      'Myst: The Book of D\'ni',
+      'MYSTlore',
+      'The Myst Reader',
+      'Myst',
+      '"Uru Live"',
+      'Uru Live',
+      'Uru: Ages Beyond Myst',
+      'Uru: To D\'ni',
+      'Uru: The Path Of The Shell',
+      'Uru',
+      'MORE',
+      'MO:UL',
+      'D\'ni',
+      'Riven',
+      'GameTap',
+      'Plasma20',
+      'Plasma',
+      '3DS Max',
+      'Guild of Messengers',
+      'Guild of Writers'
+    ]
+
+    def run(content)
+      @regex ||= Regexp.union([ /<h\d>/, /<\/h\d>/, /</, />/, WORDS.map { |w| Regexp.new(Regexp.escape(w)) }].flatten)
+
+      inside_element = false
+      inside_header  = false
+
+      content_new = content.gsub(@regex) do |match|
+        if match == '<'
+          inside_element = true
+          match
+        elsif match == '>'
+          inside_element = false
+          match
+        elsif match =~ /<h\d>/
+          inside_header = true
+          match
+        elsif match =~ /<\/h\d>/
+          inside_header = false
+          match
+        else
+          inside_element || inside_header ? match : "<i>#{match}</i>"
+        end
+      end
+
+      content_new
+    end
+
+  end
+
+end
