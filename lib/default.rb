c7d2f916b8e251cb5a40dbc02f18b727b74ec1dd
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-19 19:01:58 +0100

    added website_of and translated_guild_name helper functions

--- lib/default.rb
+++ lib/default.rb
@@ -8,9 +8,25 @@ include Nanoc::Helpers::XMLSitemap
 # Module in which all MO:UL site-specific helpers reside
 module MOULSiteHelpers
 
+  WEBSITE_URLS = {
+    :guild_of_maintainers => 'http://www.guildofwriters.com/',
+    :guild_of_messengers  => 'http://www.guildofmessengers.com/',
+    :guild_of_writers     => 'http://www.guildofmaintainers.org/'
+  }
+
   # Returns the asset with the given asset ID.
   def asset(asset_id)
     @assets.find { |asset| asset.asset_id == asset_id }
+  end
+
+  # Returns the web site for the given identifier.
+  def website_of(id)
+    WEBSITE_URLS[id]
+  end
+
+  # Returns the properly marked up translated guild name.
+  def translated_guild_name(id, translated, original)
+    link_to(translated, website_of(id), :title => original) + %[ (<span lang="en">#{original}</span>)]
   end
 
 end
