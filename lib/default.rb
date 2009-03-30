96446e314da1901f7dd0dfa28cdc05020c3aa1b0
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-30 16:20:39 +0200

    making site nanoc 2.2-only

--- lib/default.rb
+++ lib/default.rb
@@ -1,3 +1,13 @@
+# Make sure we're using nanoc 2.2 or higher
+if Nanoc::VERSION < '2.2'
+  puts 'WARNING:'
+  puts 'You need nanoc 2.2 or higher to compile this site. If you really ' +
+       'want to compile this site with an older version of nanoc, set ' +
+       'FORCE to true in your environment.'
+
+  exit unless ENV['FORCE'] == 'true'
+end
+
 # Module in which all MO:UL site-specific helpers reside
 module MOULSiteHelpers
 
