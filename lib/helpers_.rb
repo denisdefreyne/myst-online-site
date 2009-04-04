e050f667d96b16b88b45717e04f2e8ae521ebd16
Denis Defreyne <denis.defreyne@stoneship.org>
2009-04-04 17:55:54 +0200

    removed "custom" section in helpers requires

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -1,9 +1,6 @@
-# Built-in
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::HTMLEscape
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Render
 include Nanoc::Helpers::XMLSitemap
-
-# Custom
 include Nanoc::Helpers::Filtering
