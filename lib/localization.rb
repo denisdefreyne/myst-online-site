f30126f48a162626b3d216f0e5def2ed67d9a368
Denis Defreyne <denis.defreyne@stoneship.org>
2009-04-22 09:11:27 +0200

    moved translations into lib/locales/*.yaml

--- lib/localization.rb
+++ lib/localization.rb
@@ -11,101 +11,6 @@ module MOULSiteHelpers::Localization
     'nl' => 'Dutch'
   }
 
-  # 'en' => 'Myst Online: Uru Live is a soon to be open-sourced massive multiplayer online adventure game.'
-  # 'de' => 'Myst Online: Uru Live ist ein Massive-Multiplayer-Online-Adventurespiel, das in Kürze als Open Source freigegeben werden soll.'
-  # 'es' => 'Myst Online: Uru Live es un juego de aventura multijugador online masivo a punto de convertirse a código abierto.'
-  # 'fi' => 'Myst Online: Uru Live tulee pian olemaan avoimeen lähdekoodiin perustuva massiivinen seikkailu-verkkomoninpeli.'
-  # 'fr' => 'Myst Online: Uru Live sera bientôt un jeu d'aventure en ligne multijoueurs à code source ouverte.'
-  # 'it' => 'Myst Online: Uru Live è un Massively Multiplayer Online Game che sarà presto Open Source.'
-  # 'nl' => 'Myst Online: Uru Live is een massive multiplayer online adventure-spel dat binnenkort open-source gemaakt zal worden.'
-
-  # English strings and their translations in different languages.
-  TRANSLATED_STRINGS = {
-    'English translation' => {
-      'de' => 'Englische Übersetzung',
-      'es' => 'Traducción en inglés',
-      'fi' => 'Käännös englanniksi',
-      'fr' => 'Version anglaise',
-      'it' => 'Traduzione Inglese',
-      'nl' => 'Engelse vertaling'
-    },
-    'German translation' => {
-      'de' => 'Deutsche Übersetzung',
-      'es' => 'Traducción en alemán',
-      'fi' => 'Käännös saksaksi',
-      'fr' => 'Version allemande',
-      'it' => 'Traduzione Tedesca',
-      'nl' => 'Duitse vertaling'
-    },
-    'Spanish translation' => {
-      'de' => 'Spanische Übersetzung',
-      'es' => 'Traducción en español',
-      'fi' => 'Käännös espanjaksi',
-      'fr' => 'Version espagnole',
-      'it' => 'Traduzione Spagnola',
-      'nl' => 'Spaanse vertaling'
-    },
-    'Finnish translation' => {
-      'de' => 'Finnische Übersetzung',
-      'es' => 'Traducción en finés',
-      'fi' => 'WARNING: untranslated',
-      'fr' => 'Version finlandaise',
-      'it' => 'Traduzione Finlandese',
-      'nl' => 'Finse vertaling'
-    },
-    'French translation' => {
-      'de' => 'Französische Übersetzung',
-      'es' => 'Traducción en francés',
-      'fi' => 'Käännös ranskaksi',
-      'fr' => 'Version française',
-      'it' => 'Traduzione Francese',
-      'nl' => 'Franse vertaling'
-    },
-    'Italian translation' => {
-      'de' => 'Italienische Übersetzung',
-      'es' => 'Traducción en italiano',
-      'fi' => 'Käännös italiaksi',
-      'fr' => 'Version italienne',
-      'it' => 'Traduzione Italiana',
-      'nl' => 'Italiaanse vertaling'
-    },
-    'Dutch translation' => {
-      'de' => 'Niederländische Übersetzung',
-      'es' => 'Traducción en holandés',
-      'fi' => 'Käännös hollanniksi',
-      'fr' => 'Version néerlandaise',
-      'it' => 'Traduzione Olandese',
-      'nl' => 'Nederlandse vertaling'
-    },
-    '<GAME DESCRIPTION>' => {
-      'en' => 'Myst Online: Uru Live is a soon to be open-sourced massive multiplayer online adventure game.',
-      'de' => 'Myst Online: Uru Live ist ein Massive-Multiplayer-Online-Adventurespiel, das in Kürze als Open Source freigegeben werden soll.',
-      'es' => 'Myst Online: Uru Live es un juego de aventura multijugador online masivo a punto de convertirse a código abierto.',
-      'fi' => 'Myst Online: Uru Live tulee pian olemaan avoimeen lähdekoodiin perustuva massiivinen seikkailu-verkkomoninpeli.',
-      'fr' => 'Myst Online: Uru Live sera bientôt un jeu d\'aventure en ligne multijoueurs à code source ouverte.',
-      'it' => 'Myst Online: Uru Live è un Massively Multiplayer Online Game che sarà presto Open Source.',
-      'nl' => 'Myst Online: Uru Live is een massive multiplayer online adventure-spel dat binnenkort open-source gemaakt zal worden.'
-    },
-    '<FEED TITLE>' => {
-      'de' => 'Myst Online: Uru Live news',
-      'en' => 'Myst Online: Uru Live news',
-      'es' => 'Noticias de Myst Online: Uru Live',
-      'fi' => 'Myst Online: Uru Live uutisia',
-      'fr' => 'Nouvelles de Myst Online: Uru Live',
-      'it' => 'Notizie di Myst Online: Uru Live',
-      'nl' => 'Myst Online: Uru Live nieuws'
-    },
-    '<FEED URL>' => {
-      'de' => 'http://feeds2.feedburner.com/uru_de',
-      'en' => 'http://feeds2.feedburner.com/uru',
-      'es' => 'http://feeds2.feedburner.com/uru_es',
-      'fi' => 'http://feeds2.feedburner.com/uru_fi',
-      'fr' => 'http://feeds2.feedburner.com/uru_fr',
-      'it' => 'http://feeds2.feedburner.com/uru_it',
-      'nl' => 'http://feeds2.feedburner.com/uru_nl'
-    }
-  }
-
   # Returns the item's language code
   def language_code_of(item)
     item.language_code || (item.path.match(/^\/(..)\//) || [])[1]
@@ -136,9 +41,16 @@ module MOULSiteHelpers::Localization
     # Don't translate English texts
     return string if dst_lang == 'en'
 
-    # Translate
-    translations = TRANSLATED_STRINGS[string] || {}
-    translations[dst_lang] || 'WARNING: untranslated'
+    # Try translating using locales
+    # FIXME do not use a global variable
+    unless defined?($locales)
+      $locales = {}
+      Dir['lib/locales/*.yaml'].each do |locale_filename|
+        locale_name = locale_filename.sub(%r{^lib/locales/(\w+)\.yaml}, '\1')
+        $locales[locale_name.to_sym] = YAML.load_file(locale_filename)
+      end
+    end
+    ($locales[dst_lang.to_sym] || {})[string]
   end
 
   module TimeExtensions
